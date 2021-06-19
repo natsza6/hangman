32d4cc6de0f2bef2351420cdffd24bc358e1075a
natsza6 <83343731+natsza6@users.noreply.github.com>
2021-06-20 01:12:54 +0200

    zmiana sposobu kodowania na zmieniane stronek gry

--- game.py
+++ game.py
@@ -3,43 +3,60 @@ from tkinter import messagebox
 from tkinter.ttk import *
 from PIL import ImageTk, Image
 
-#okienko gry
+#główne okienko gry
 window = Tk()
 window.geometry('600x600')
 window.title('Stu(dying)')
 window.config(bg = '#FFFFFF')
 
-#logo
-logo_img = ImageTk.PhotoImage(Image.open('/Users/mac/Desktop/logo.png'))
-logo = Label(window, image=logo_img)
-logo.pack()
-
-#wczytanie wszystkich grafik
+#wczytanie grafik
 noc_przed_img = ImageTk.PhotoImage(Image.open('/Users/mac/Desktop/noc_przed.png'))
 bez_spiny_img = ImageTk.PhotoImage(Image.open('/Users/mac/Desktop/bez_spiny.png'))
 choose_your_fighter_img = ImageTk.PhotoImage(Image.open('/Users/mac/Desktop/choose_your_fighter.png'))
+logo_img = ImageTk.PhotoImage(Image.open('/Users/mac/Desktop/logo.png'))
 
-def Open_Menu():
+#PIERWSZA STRONA GRY
+def tab1():
+
+    #DRUGA STRONA GRY
+    def tab2():
+        label1.destroy()
+        button1.destroy()
+        button2.destroy()
+        label2 = Label(window, image = choose_your_fighter_img)
+        label2.pack(pady=10)
+        options = [
+        "Wrogowie Studentów",
+        "Paliwo Studenta",
+        "Energetyki"
+        ]
+        clicked = StringVar()
+        clicked.set(options[0])
+
+        drop =  OptionMenu(window, clicked, *options)
+        drop.pack(pady=20)
+
+        button3 = Button(window, text = "OK")
+        button3.pack(pady=10)
 
 
-    menu = Toplevel(window)
-    menu.title("Stu(dying)")
-    menu.geometry('600x600')
-    logo = Label(menu, image=choose_your_fighter_img)
-    logo.pack()
+        def tab3():
+            label2.destroy()
+            button2.destroy()
+            label1.destroy()
+            button1.destroy()
+            tab1()
 
-label = Label(window)
-label.pack(pady=10)
+    #CZĘŚĆ DALSZA PIERWSZEJ STRONY GRY
+    label1 = Label(window, image=logo_img)
+    label1.pack(pady=10)
 
-#przyciski
-button_noc_przed = Button(window, image=noc_przed_img, command=Open_Menu)
-button_noc_przed.pack()
+    button1 = Button(window, image = noc_przed_img,command=tab2)
+    button1.pack(pady=10)
 
-button_bez_spiny = Button(window, image=bez_spiny_img, command=Open_Menu)
-button_bez_spiny.pack()
-
-
+    button2 = Button(window, image = bez_spiny_img,command=tab2)
+    button2.pack(pady=10)
 
 
+tab1()
 window.mainloop()
-
